fde30bb9ffdce60f0d4cfa72cc89170300a4b1e1
Alyona <oranchuka@gmail.com>
2025-08-29 23:06:12 +0300

    push

--- app/notes/Notes.Client.tsx
+++ app/notes/Notes.Client.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useState, useEffect } from 'react';
 import { useDebounce, useDebouncedCallback } from 'use-debounce';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,7 +3,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import NotesClient from './Notes.Client';
+import NotesClient from './Notes.client';
 import { fetchNotes } from '@/lib/api';
 
 export default async function App() {
