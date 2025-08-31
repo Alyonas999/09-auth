098e38847cf16b49c79473a9a09a34b9d8ec3b99
Alyona <oranchuka@gmail.com>
2025-09-01 00:32:55 +0300

    fix

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import css from "./NoteForm.module.css";
+import css from './NoteForm.module.css';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
