be757fc0e55100e9b8cc5183d360482669f45d05
Alyona <oranchuka@gmail.com>
2025-09-06 00:57:20 +0300

    page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,38 +1,39 @@
 import {
-	dehydrate,
-	HydrationBoundary,
-	QueryClient,
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
 } from "@tanstack/react-query"
 import { fetchNotes, getCategories, Tags } from "@/lib/api"
 import NotesClient from "./Notes.client"
-
-interface NotesFilterProps {
-  params: { slug: string[] }
-  searchParams?: { [key: string]: string | string[] | undefined }
-}
 
 export const dynamicParams = false
 export const revalidate = 900
 
 export const generateStaticParams = async () => {
-	const categories = await getCategories()
-	return categories.map(category => ({ slug: [category] }))
+  const categories = await getCategories()
+  return categories.map(category => ({ slug: [category] }))
 }
 
-export default async function NotesFilter({ params }: NotesFilterProps) {
-	const queryClient = new QueryClient()
-	const categories = await getCategories()
-	const { slug } = params
-	const category = slug[0] === "All" ? undefined : slug[0]
+export default async function NotesFilter({
+  params,
+  searchParams,
+}: {
+  params: { slug: string[] }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+  const queryClient = new QueryClient()
+  const categories = await getCategories()
+  const { slug } = params
+  const category = slug[0] === "All" ? undefined : slug[0]
 
-	await queryClient.prefetchQuery({
-		queryKey: ["notes", { search: "", page: 1, category }],
-		queryFn: () => fetchNotes("", 1, category),
-	})
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", { search: "", page: 1, category }],
+    queryFn: () => fetchNotes("", 1, category), 
+  })
 
-	return (
-		<HydrationBoundary state={dehydrate(queryClient)}>
-			<NotesClient categories={categories} category={category} />
-		</HydrationBoundary>
-	)
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient categories={categories} category={category} />
+    </HydrationBoundary>
+  )
 }
