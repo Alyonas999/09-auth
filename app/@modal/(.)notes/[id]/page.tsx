9513819d25c6d405973e25312bd526f281aa4f91
Alyona <oranchuka@gmail.com>
2025-09-10 16:55:23 +0300

    fix

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,19 +1,14 @@
+
 import { fetchNoteById } from "@/lib/api"
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query"
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
 import NotePreviewClient from "./NotePreview.client"
 
-interface PageProps {
-  params: { id: string }
-}
 
-const NotePreview = async ({ params }: PageProps) => {
-  const { id } = params 
+const NotePreview = async ({ params }: { params: { id: string } }) => {
+  const { id } = params
   const queryClient = new QueryClient()
-    await queryClient.prefetchQuery({
+
+  await queryClient.prefetchQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
   })
