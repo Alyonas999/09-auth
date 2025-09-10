58bed231b48cc8fcd7b479ba4e029276de2bec80
Alyona <oranchuka@gmail.com>
2025-09-10 17:32:30 +0300

    page

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,26 +1,29 @@
-
 import { fetchNoteById } from "@/lib/api"
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
+import {
+	dehydrate,
+	HydrationBoundary,
+	QueryClient,
+} from "@tanstack/react-query"
 import NotePreviewClient from "./NotePreview.client"
 
 type Props = {
-  params: { id: string }
+	params: Promise<{ id: string }>
 }
 
 const NotePreview = async ({ params }: Props) => {
-  const { id } = params
-  const queryClient = new QueryClient()
+	const { id } = await params
+	const queryClient = new QueryClient()
 
-  await queryClient.prefetchQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
-  })
+	await queryClient.prefetchQuery({
+		queryKey: ["note", id],
+		queryFn: () => fetchNoteById(id),
+	})
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotePreviewClient id={id} />
-    </HydrationBoundary>
-  )
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<NotePreviewClient id={id} />
+		</HydrationBoundary>
+	)
 }
 
 export default NotePreview
