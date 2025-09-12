52ab2a6dd5231717f901deb64985cb1b1b62d353
Alyona <oranchuka@gmail.com>
2025-09-12 13:51:04 +0300

    filter

--- app/notes/filter/page.tsx
+++ app/notes/filter/page.tsx
@@ -5,14 +5,19 @@ import {
 } from "@tanstack/react-query";
 import { fetchNotes, getCategories } from "@/lib/api";
 import NotesClient from "@/app/notes/filter/[...slug]/Notes.client";
+
 export default async function NotesPage() {
   const queryClient = new QueryClient();
-  const categories = getCategories;
+  
+
+  const categories = await getCategories();
   const category = undefined;
+
   await queryClient.prefetchQuery({
-    queryKey: ["notes", { search: "", page: 1, category }],
-    queryFn: () => fetchNotes("", 1, undefined, category),
+    queryKey: ["notes", { search: "", page: 1, tag: category }], 
+    queryFn: () => fetchNotes("", 1, 12, category), 
   });
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NotesClient categories={categories} category={category} />
